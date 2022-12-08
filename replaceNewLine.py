82bebee6821a64c9c46cf947d90fae7c1d7d96d6
vilanmarius <vilanmarius80@gmail.com>
2022-12-08 22:22:22 +0200

    Commit just to keep in case i need this logic

---
+++ replaceNewLine.py
@@ -0,0 +1,9 @@
+#logic to created to try to replace newline with the replace method
+from time import sleep
+for i in range(30):
+    sleep(0.1)
+    print(".",end=' ')
+
+#not working like i thought
+#the inital ideea was to try like in some previous logic
+#print(".".replace("\n",''))
